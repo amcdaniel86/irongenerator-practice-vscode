00e27c1c98563a7c1676c43faa3b16ce1c9c5850
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-14 21:41:24 -0500

    update5

--- models/Book.js
+++ models/Book.js
@@ -4,13 +4,19 @@ const Schema =   mongoose.Schema;
 const bookSchema = new Schema({
     name: String,
     description: String,
-    author: String,
+    author: [ { type : _Id, ref: 'Author' } ],
     rating: Number
+    reviews: [ 
+  {
+    user: String,
+    comments: String
+  } 
+]
 }, {
-  timestamps: {
-    createdAt:"created_at",
-    updatedAt: "updated_at"
-  }
+timestamps: {
+  createdAt: "createdAt",
+  updatedAt: "updatedAt"
+}
 });
 
 const Book = mongoose.model("Book", bookSchema);
--- routes/index.js
+++ routes/index.js
@@ -35,20 +35,20 @@ router.get('/books/add', (req, res, next) => {
 });
 
 router.post('/books/add', (req, res, next) => {
-    const { name, author, description, rating } = req.body;
-    const newBook = new Book({ name, author, description, rating});
-      newBook.save()
-        .then((book) =>{
-          res.redirect('/books');
-        })
-        .catch((err) =>{
-          cconsole.log(err);
-        })
-});
-
-// Query String method below, notice req.query
-router.get('/books/edit', (req, res, next) =>{
-  Book.findOne({_id: req.query.book_id})
+  const { name, author, description, rating } = req.body;
+  const newBook = new Book({ name, author, description, rating});
+  newBook.save()
+  .then((book) =>{
+    res.redirect('/books');
+  })
+  .catch((err) =>{
+        cconsole.log(err);
+    })
+  });
+  
+  // Query String method below, notice req.query
+  router.get('/books/edit', (req, res, next) =>{
+    Book.findOne({_id: req.query.book_id})
   .then((book) => {
     res.render("book-edit", {book});
   })
@@ -67,6 +67,63 @@ router.post('/books/edit', (req, res, next) => {
     console.log(err);
   })
 });
+
+router.get('/authors/add', (req, res, next) =>{
+  res.render("author-add")
+});
+
+
+
+router.post('/authors/add', (req, res, next) =>{
+  const { name, lastName, nationality, birthday, pictureUrl } = req.body;
+  const newAuthor = new Author ({name, lastName, nationality, birthday, pictureUrl})
+  newAuthor.save()
+  .then((book)=>{
+    res.redirect('/books')
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+});
+router.post('/books/add', (req, res, next) => {
+  const { name, author, description, rating } = req.body;
+  const newBook = new Book({ name, author, description, rating});
+  newBook.save()
+  .then((book) =>{
+    res.redirect('/books');
+  })
+  .catch((err) =>{
+        cconsole.log(err);
+    })
+
+    router.get('/book/:id', (req, res, next) => {
+      let bookId = req.params.id;
+      if (!/^[0-9a-fA-F]{24}$/.test(bookId)) { 
+        return res.status(404).render('not-found');
+      }
+      Book.findOne({'_id': bookId})
+        .populate('author')
+        .then(book => {
+          if (!book) {
+              return res.status(404).render('not-found');
+          }
+          res.render("book-detail", { book })
+        })
+        .catch(next)
+    });
+
+    router.post('/reviews/add', (req, res, next) => {
+      const { user, comments } = req.body;
+      Book.update({ _id: req.query.book_id }, { $push: { reviews: { user, comments }}})
+      .then(book => {
+        res.redirect('/books')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    });
+
+
 
 
 module.exports = router;
